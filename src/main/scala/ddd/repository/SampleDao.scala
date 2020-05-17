08d20b68f41764d0631e8badbff8fbefbd7b01cc
Kazuya Oki <3143141@kuis.ac.jp>
2020-05-17 15:03:45 +0900

    slickの実装を追加

--- src/main/scala/ddd/repository/SampleDao.scala
+++ src/main/scala/ddd/repository/SampleDao.scala
@@ -25,35 +25,14 @@ object MySQLDBAction extends MySQLConnector {
   (action: T#SlickQuery => DBIOAction[A, NoStream, Nothing]): Future[A] = {
     for {
       db    <- getDatabase(table.baseConfigMap(host))
-      value <- invoke[T, A](table, host, {
-        slick => db.run(action(slick))
-      })
-    } yield value
-  }
-  def invoke[T <: MySQLTable[_], A](table: T, host: String, block: T#SlickQuery => Future[A]): Future[A] = {
-    for {
-      db <- getDatabase(table.baseConfigMap(host))
-      value <- block(table.query)
+      value <- db.run(action(table.query))
     } yield value
   }
 }
 
 object Sample2 {
   import com.typesafe.config._
-  def hoge(s: String)(str: String => Future[String]): Future[String] = {
-    str(s)
-  }
-  hoge("d"){ str => 
-    print(str)
-    Future(str)
-  }
-  def main(args: Array[String]): Unit = {
-     hoge("d"){ str => 
-    print(str)
-    Future(str)
-  }
-    val conf = ConfigFactory.load()
-    println(conf.getString("mysql.test.host_master"))
-    println(conf.getString("mysql.test.databaseName"))
-  }
+  val conf = ConfigFactory.load()
+  println(conf.getString("mysql.test.host_master"))
+  println(conf.getString("mysql.test.databaseName"))
 }
